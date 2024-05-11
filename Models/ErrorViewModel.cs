d3fd2dd0f090441585ddf8d288326894cdedb880
jazmindestefano <jazmin.destefano@gm2dev.com>
2024-05-11 11:06:01 -0300

    feat: create dot net project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegistroDeVentas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
